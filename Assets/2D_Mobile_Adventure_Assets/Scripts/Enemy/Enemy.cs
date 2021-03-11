068de313dcece1a3835ceaa808dd2883a6b0dc2d
agdodson <andy.dodson@outlook.com>
2021-03-11 10:24:29 +0200

    - Added Inheritance.
    -- MossGaint & Spider inherit base movement from Enemy Class

--- Assets/2D_Mobile_Adventure_Assets/Scripts/Enemy/Enemy.cs
+++ Assets/2D_Mobile_Adventure_Assets/Scripts/Enemy/Enemy.cs
@@ -14,17 +14,78 @@ public abstract class Enemy : MonoBehaviour
 	[SerializeField] protected Transform pointA;
 	[SerializeField] protected Transform pointB;
 
-	
+	protected Vector3 destination;
+	protected Animator enemyAnim;
+	protected SpriteRenderer enemySprite;
+	protected bool flip;
 
-	protected abstract void Update();
+	private void Start()
+	{
+		// Important componenets for enemy class. 
+		enemyAnim = GetComponentInChildren<Animator>();
+		if (enemyAnim == null)
+		{
+			Debug.LogError("No Animator Component found on " + name);
+		}
+		enemySprite = GetComponentInChildren<SpriteRenderer>();
+		if (enemySprite == null)
+		{
+			Debug.LogError("No Sprite Renderer found on " + name);
+		}
+		Init();
+	}
 
-	
+	protected virtual void Init()
+	{
+		// Use for Custom Start Initalistion of enemys.
+	}
+
+	protected virtual void Update()
+	{
+		// If enemy is idle, do nothing.
+		if (enemyAnim.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+		{
+			return;
+		}
+		if (flip)
+		{
+			enemySprite.flipX = !enemySprite.flipX;
+			flip = false;
+		}
+		EnemyMovement();
+	}
+
+	protected virtual void EnemyMovement()
+	{
+		// Enemy Base Movement.
+		// Move enemy between points A & B.
+
+		transform.position = Vector3.MoveTowards(transform.position, destination, speed * Time.deltaTime);
+		/* On arrival at destination point
+		 * Change destination point,
+		 * Trigger Idle animation
+		 * Set flip to true
+		 */
+		if (Vector3.Distance(transform.position, destination) < 0.1f)
+		{
+			if (destination == pointA.position)
+			{
+				enemyAnim.SetTrigger("Idle");
+				destination = pointB.position;
+				flip = true;
+			}
+			else if (destination == pointB.position)
+			{
+				enemyAnim.SetTrigger("Idle");
+				destination = pointA.position;
+				flip = true;
+			}
+		}
+	}
 
 	public virtual void Attack()
 	{
 		Debug.Log("BaseAttackCalled");
-	}
-
-	
+	} 
 
 }
--- Assets/2D_Mobile_Adventure_Assets/Scripts/Enemy/Spider.cs
+++ Assets/2D_Mobile_Adventure_Assets/Scripts/Enemy/Spider.cs
@@ -4,72 +4,9 @@ using UnityEngine;
 
 public class Spider : Enemy
 {
-	private Vector3 _destination;
-	private Animator _enemyAnim;
-	private SpriteRenderer _enemySprite;
-	private bool _flip;
-
-
-	private void Start()
+	protected override void Init()
 	{
-		// Get componets for enemy.
-		_enemyAnim = GetComponentInChildren<Animator>();
-		if (_enemyAnim == null)
-		{
-			Debug.LogError("No Animator Component found on " + name);
-		}
-		_enemySprite = GetComponentInChildren<SpriteRenderer>();
-		if (_enemySprite == null)
-		{
-			Debug.LogError("No Sprite Renderer found on " + name);
-		}
-		_destination = pointB.position;
-	}
-
-	protected override void Update()
-	{
-		EnemyUpdate();
-	}
-
-	private void EnemyMovement()
-	{
-		// Move enemy between points A & B.
-
-		transform.position = Vector3.MoveTowards(transform.position, _destination, speed * Time.deltaTime);
-		/* On arrival at destination point
-		 * Change destination point,
-		 * Trigger Idle animation
-		 * Set flip to true
-		 */
-		if (Vector3.Distance(transform.position, _destination) < 0.1f)
-		{
-			if (_destination == pointA.position)
-			{
-				_enemyAnim.SetTrigger("Idle");
-				_destination = pointB.position;
-				_flip = true;
-			}
-			else if (_destination == pointB.position)
-			{
-				_enemyAnim.SetTrigger("Idle");
-				_destination = pointA.position;
-				_flip = true;
-			}
-		}		
-	}
-
-	private void EnemyUpdate()
-	{
-		// If enemy is idle, do nothing.
-		if (_enemyAnim.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
-		{
-			return;
-		}
-		if (_flip)
-		{
-			_enemySprite.flipX = !_enemySprite.flipX;
-			_flip = false;
-		}
-		EnemyMovement();
+		base.Init();
+		destination = pointB.position;
 	}
 }
